057a4d5f43ca71fbe4e2798689368f970896d817
228846 <ekasekv@gmail.com>
2022-06-30 09:19:59 -0700

    Tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+print("Size of a char: %d byte(s)\n", size of(char));
+print("Size of an int: %d byte(s)\n", size of(int));
+print("Size of a long int: %d byte(s)\n", size of(long int));
+print("Size of a long long int: %d byte(s)\n", size of(long long int));
+print("Size of a float: %d byte(s)\n", size of(float));
+return (0);
+}
---
+++ test.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+printf("Something");
+return 0;
+}

bf77ffbc33e4b6de6829f00e9917eca25d09e158
Kasasira Enoch <ekasekv@gmail.com>
2022-07-05 22:25:22 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print alphabet -> prints lowercase alphabets
+ */
+void print_alphabet(void)
+{
+    int j;
+    for (j = 'a'; j <= 'z'; j++)
+    {
+    _putchar(j);
+    }
+    _putchar('\n')
+
+}

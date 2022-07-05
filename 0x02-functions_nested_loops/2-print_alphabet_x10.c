98ebaf9ca6ec9bd0025b74dcd169a9156344e0ba
Kasasira Enoch <ekasekv@gmail.com>
2022-07-05 23:00:16 +0300

    tasks 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print alphabet_x10-> prints lowercase alphabets
+ */
+void print_alphabet_x10(void);
+{
+    int j;
+    int x;
+    for (j = 'a'; j <= 'z'; j++)
+    {
+        for (j = 'a'; j <= 'z'; j++)
+        {
+        _putchar(j);
+        }
+        _putchar('\n');
+
+    }
+}

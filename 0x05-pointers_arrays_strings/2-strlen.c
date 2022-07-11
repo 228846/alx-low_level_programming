81f59462c6bcc372897b0727ad2376df9ec2b9cc
Kasasira Enoch <ekasekv@gmail.com>
2022-07-11 18:32:13 +0300

    Tasks

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _strlen -> to get the length of the string
+ * @s: string pointer to passed to this function
+ * Return: returns length of the string
+ */
+int _strlen(char *s)
+{
+    return (strlen(s));
+} 
